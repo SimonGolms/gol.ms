3b61796ed8daf7a28eb8a5908acc8e594cd69d5d
Simon G <development@gol.ms>
2021-05-27 20:11:28 +0200

    fix: set body background color to match image

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,12 +1,12 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document';
 import React from 'react';
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
         <Head />
-        <body>
+        <body className="bg-white-image dark:bg-black-image">
           <Main />
           <NextScript />
         </body>
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,10 @@ module.exports = {
   darkMode: 'class',
   theme: {
     extend: {
+      backgroundColor: () => ({
+        'black-image': '#141414',
+        'white-image': '#d4d4d4',
+      }),
       backgroundImage: () => ({
         'hero-dark': "url('/assets/images/background-dark.jpg')",
         'hero-light': "url('/assets/images/background-light.jpg')",
