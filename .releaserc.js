4e3b10075c637b7a08fe5bf5d62e82607c9b55a3
Simon G <development@gol.ms>
2022-03-31 21:40:27 +0200

    ci(release): update semantic-release configuration
    - extend release rules

--- .releaserc.js
+++ .releaserc.js
@@ -1,8 +1,46 @@
 module.exports = {
   branches: ['main'],
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+        preset: 'angular',
+        releaseRules: [
+          { release: 'patch', type: 'chore' },
+          { release: 'patch', type: 'refactor' },
+          { release: 'patch', type: 'style' },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        parserOpts: {
+          noteKeywords: ['BREAKING CHANGE', 'BREAKING CHANGES', 'BREAKING'],
+        },
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { hidden: false, section: 'Build System', type: 'build' },
+            { hidden: false, section: 'Build System', type: 'chore' },
+            { hidden: false, section: 'Continuous Integration', type: 'ci' },
+            { hidden: false, section: 'Documentation', type: 'docs' },
+            { hidden: false, section: 'Features', type: 'feat' },
+            { hidden: false, section: 'Bug Fixes', type: 'fix' },
+            { hidden: false, section: 'Performance Improvements', type: 'perf' },
+            { hidden: false, section: 'Code Refactoring', type: 'refactor' },
+            { hidden: false, section: 'Styles', type: 'style' },
+            { hidden: false, section: 'Tests', type: 'test' },
+          ],
+        },
+        writerOpts: {
+          commitsSort: ['subject', 'scope'],
+        },
+      },
+    ],
     '@semantic-release/changelog',
     ['@semantic-release/npm', { npmPublish: false }],
     '@semantic-release/github',
